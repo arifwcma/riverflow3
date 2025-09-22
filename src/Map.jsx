22df931d9428717a97b90e9d36bf49f19abca0c4
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-09-22 16:46:31 +1000

    done

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,17 +1,34 @@
-import React from "react"
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import React, { useEffect, useState } from "react"
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMap } from "react-leaflet"
 import MockData from "./MockData.js"
 
-function Map() {
-    const bounds = [
-        [-37.36809668716929, 140.96378317173532],
-        [-35.65680550597246, 143.36396129024806]
-    ]
+function BoundaryLayer({ boundary }) {
+    const map = useMap()
+
+    useEffect(() => {
+        if (boundary) {
+            const bounds = L.geoJSON(boundary).getBounds()
+            map.fitBounds(bounds)
+        }
+    }, [boundary, map])
+
+    return (
+        <GeoJSON data={boundary} style={{ color: "blue", weight: 2, fillOpacity: 0 }} />
+    )
+}
+
+export default function Map() {
+    const [boundary, setBoundary] = useState(null)
+
+    useEffect(() => {
+        fetch("/wcma_boundary.geojson")
+            .then(res => res.json())
+            .then(data => setBoundary(data))
+    }, [])
 
     return (
         <MapContainer
             style={{ height: "600px", width: "100%" }}
-            bounds={bounds}
             zoomControl={false}
             dragging={false}
             scrollWheelZoom={false}
@@ -24,6 +41,7 @@ function Map() {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="© OpenStreetMap contributors"
             />
+            {boundary && <BoundaryLayer boundary={boundary} />}
             {MockData.map((loc, idx) => (
                 <Marker key={idx} position={[loc.lat, loc.lng]}>
                     <Popup>{loc.name}</Popup>
@@ -32,5 +50,3 @@ function Map() {
         </MapContainer>
     )
 }
-
-export default Map
