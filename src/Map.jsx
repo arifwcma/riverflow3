163f7fdc19a3e0d69e821f0dedb45fc6c63b3eea
arif.rahman <arif.rahman@wcma.vic.gov.au>
2025-09-23 13:36:28 +1000

    done

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,21 +1,32 @@
 import React, { useEffect, useState } from "react"
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMap } from "react-leaflet"
-import MockData from "./MockData.js"
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet"
+import L from "leaflet"
+import stations from "../stations_15.json"
+import stationStatus from "../station_status.json"
 
-function BoundaryLayer({ boundary }) {
-    const map = useMap()
+const bounds = [
+    [-37.36809668716929, 140.96378317173532],
+    [-35.65680550597246, 143.36396129024806]
+]
 
-    useEffect(() => {
-        if (boundary) {
-            const bounds = L.geoJSON(boundary).getBounds()
-            map.fitBounds(bounds)
-        }
-    }, [boundary, map])
+const activeIcon = new L.Icon({
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    shadowSize: [41, 41]
+})
 
-    return (
-        <GeoJSON data={boundary} style={{ color: "blue", weight: 2, fillOpacity: 0 }} />
-    )
-}
+const inactiveIcon = new L.Icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png",
+    iconRetinaUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+})
 
 export default function Map() {
     const [boundary, setBoundary] = useState(null)
@@ -26,9 +37,16 @@ export default function Map() {
             .then(data => setBoundary(data))
     }, [])
 
+    const statusLookup = stationStatus.stations.reduce((acc, s) => {
+        acc[s.name] = s.status
+        return acc
+    }, {})
+
     return (
         <MapContainer
             style={{ height: "600px", width: "100%" }}
+            bounds={bounds}
+            maxBounds={bounds}
             zoomControl={false}
             dragging={false}
             scrollWheelZoom={false}
@@ -37,16 +55,33 @@ export default function Map() {
             keyboard={false}
             touchZoom={false}
         >
+
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="© OpenStreetMap contributors"
             />
-            {boundary && <BoundaryLayer boundary={boundary} />}
-            {MockData.map((loc, idx) => (
-                <Marker key={idx} position={[loc.lat, loc.lng]}>
-                    <Popup>{loc.name}</Popup>
-                </Marker>
-            ))}
+            {boundary && (
+                <GeoJSON
+                    data={boundary}
+                    style={{ color: "blue", weight: 2, fillOpacity: 0 }}
+                />
+            )}
+            {stations.map((loc, idx) => {
+                const isActive = statusLookup[loc.station_name] === 1
+                return (
+                    <Marker
+                        key={idx}
+                        position={[parseFloat(loc.station_latitude), parseFloat(loc.station_longitude)]}
+                        icon={isActive ? activeIcon : inactiveIcon}
+                    >
+                        <Popup>
+                            <b>{loc.station_name}</b>
+                            <br />
+                            Status: {isActive ? "Active" : "Inactive"}
+                        </Popup>
+                    </Marker>
+                )
+            })}
         </MapContainer>
     )
 }
